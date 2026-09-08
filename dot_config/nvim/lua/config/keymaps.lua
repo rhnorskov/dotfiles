9f0a0a44fafdd541910d17cbd0309498af4eb7a8
Rasmus Nørskov <rn@onskeskyen.dk>
2026-09-08 08:50:07 +0200

    feat(nvim): add option+backspace, drop hscroll and inlay hints

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- macOS Option+Backspace: delete the word before the cursor.
+vim.keymap.set({ "i", "c" }, "<M-BS>", "<C-w>")
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Trackpads emit stray horizontal scroll events that lurch the view sideways.
+-- 0 disables horizontal mouse scrolling; use <leader>uw or $ to reach long lines.
+vim.opt.mousescroll = "ver:3,hor:0"
